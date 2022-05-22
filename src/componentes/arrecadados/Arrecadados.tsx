dbcb11e037bdeda0b53c3602ae0a620b22917c9a
Raphael <raphaelrodrigues93@gmail.com>
2022-05-22 20:58:58 -0300

    finalizando componente Arrecadados

--- src/componentes/arrecadados/Arrecadados.tsx
+++ src/componentes/arrecadados/Arrecadados.tsx
@@ -1,9 +1,31 @@
+import './arrecadados.css'
 function Arrecadados(){
     return(
-        <div>
+        <div className="lista">
             <h1>Arrecadados</h1>
-            <h3>Arrecadamos até agora:</h3>
-            <h4>Cobertores: </h4>
+            <h2>Arrecadamos até agora:</h2>
+            <div className='categoria'>
+                <p><strong>Cobertores:</strong></p><b> 6</b>
+            </div>
+            <div className='categoria'>
+                <p><strong>Masculino</strong></p>
+                <p>Blusas de frio:</p><b> 4</b>
+                <p>Calças:</p><b> 2</b>
+            </div>
+            <div className='categoria'>
+                <p><strong>Feminino</strong></p>
+                <p>Blusas de frio:</p><b> 12</b>
+                <p>Calças:</p><b> 1</b>
+            </div>
+            <div className='categoria'>
+                <p><strong>Infantil</strong></p>
+                <p>Blusas de frio:</p><b> 9</b>
+                <p>Calças:</p><b> 6</b>
+            </div>
+            <div className='categoria'>
+                <p><strong>Valor em Dinheiro</strong></p>
+                <b>R$ 0,00</b>
+                </div>
         </div>
     )
 }
