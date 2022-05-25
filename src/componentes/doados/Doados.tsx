a28598fec7606ff21321edb3dd516908d1af35d1
Raphael <raphaelrodrigues93@gmail.com>
2022-05-24 22:28:06 -0300

    finalização de estilização Doados

--- src/componentes/doados/Doados.tsx
+++ src/componentes/doados/Doados.tsx
@@ -1,33 +1,45 @@
 import Cabecalho from '../cabecalho/Cabecalho'
+import Footer from '../footer/Footer'
 import './doados.css'
 function Doados(){
     return(
         <div className='lista-doados'>
             <Cabecalho/>
-            <h1>Doados</h1>
-            <h2>Doamos até agora:</h2>
-            <div className='categoria-doados'>
-                <p><strong>Cobertores:</strong></p><b> 6</b>
+            <div className='lista-dados-container'>
+                <h1>Doados</h1>
+                <h2>Doamos até agora:</h2>
+                <div className='container-doados'>
+                    <div className='categoria-doados'>
+                        <p><strong>Cobertores:</strong></p><b> 6</b>
+                    </div>
+                    <div className='categoria-doados'>
+                        <p><strong>Masculino</strong></p>
+                        <div>
+                            <p>Blusas de frio:</p><b> 4</b>
+                            <p>Calças:</p><b> 2</b>
+                        </div>
+                    </div>
+                    <div className='categoria-doados'>
+                        <p><strong>Feminino</strong></p>
+                        <div>
+                            <p>Blusas de frio:</p><b> 12</b>
+                            <p>Calças:</p><b> 1</b>
+                        </div>
+                    </div>
+                    <div className='categoria-doados'>
+                        <p><strong>Infantil</strong></p>
+                        <div>
+                            <p>Blusas de frio:</p><b> 9</b>
+                            <p>Calças:</p><b> 6</b>
+                        </div>
+                    </div>
+                    <div className='categoria-doados'>
+                        <p><strong>Valor em dinheiro usado</strong></p>
+                        <b>R$ 0,00</b>
+                    </div>
+                </div>
             </div>
-            <div className='categoria-doados'>
-                <p><strong>Masculino</strong></p>
-                <p>Blusas de frio:</p><b> 4</b>
-                <p>Calças:</p><b> 2</b>
-            </div>
-            <div className='categoria-doados'>
-                <p><strong>Feminino</strong></p>
-                <p>Blusas de frio:</p><b> 12</b>
-                <p>Calças:</p><b> 1</b>
-            </div>
-            <div className='categoria-doados'>
-                <p><strong>Infantil</strong></p>
-                <p>Blusas de frio:</p><b> 9</b>
-                <p>Calças:</p><b> 6</b>
-            </div>
-            <div className='categoria-doados'>
-                <p><strong>Valor em dinheiro usado</strong></p>
-                <b>R$ 0,00</b>
-            </div>
+            <Footer/>
         </div>
     )
 }
